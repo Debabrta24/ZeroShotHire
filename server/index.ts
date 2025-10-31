84a1c8ac1d07f0599d56a18668987ce38ad32282
Debabrata24 <mr.debabrtapc2006@gmail.com>
2025-10-31 18:10:10 +0530

    oth

--- server/index.ts
+++ server/index.ts
@@ -49,12 +49,18 @@ app.use(express.urlencoded({ extended: false }));
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+
+  // const port = parseInt(process.env.PORT || '5000', 10);
+  // server.listen({
+  //   port,
+  //   host: "0.0.0.0",
+  //   reusePort: true,
+  // }, () => {
+  //   log(`serving on port ${port}`);
+  // });
+
+app.listen(5010, () => {
+  console.log(`✅ Server running at http://localhost:${5010}`);
+});
+
 })();
